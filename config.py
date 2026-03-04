f0ef2ea89a989c4c871c2d7a150714b9dc949141
Antoniya Aleksandrova <27976003+AntoniyaAleksandrova@users.noreply.github.com>
2026-03-04 10:43:25 -0500

    Remove old contents (before packaging)

--- config.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-debug = False
-epsilon = 0.0001
-str_data_template_fn = None
-log_filename = 'ENCOMPASS_DEBUG_'\
-    + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")\
-    + '.txt'
